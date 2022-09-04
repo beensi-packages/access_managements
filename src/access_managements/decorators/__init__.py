f12450f3c14246ad2644767c8b76a554ba77d511
nvd <navidsoleymani@ymail.com>
2022-09-05 03:47:51 +0430

    refactoring

--- src/access_managements/decorators/__init__.py
+++ src/access_managements/decorators/__init__.py
@@ -7,21 +7,20 @@ def apikey_authorization(func):
     async def wrapper(*args, **kwargs):
         rsp = {}
         try:
-            kwargs['logger_func'].info(f"Start requesting \'{kwargs['request'].url._url}\'.")
+            kwargs['logger_func'].info('Start requesting.', kwargs['request'].url._url)
 
             if not kwargs['db'].query(kwargs['model']).filter(kwargs['model'].id == kwargs['apikey'],
                                                               kwargs['model'].is_active == True).first():
                 rsp = not_access()
-                kwargs['logger_func'].info("APIKey is not entered or the entered APIKey is not acceptable"
-                                           f"(request: {kwargs['request'].url._url}).")
+                kwargs['logger_func'].info('APIKey is not entered or the entered APIKey is not acceptable.',
+                                           kwargs['request'].url._url)
             else:
                 rsp = await func(*args, **kwargs)
         except Exception as exp:
             rsp = exception(exp.args[0])
-            kwargs['logger_func'].error(exp.args[0])
+            kwargs['logger_func'].error(exp.args[0], kwargs['request'].url._url)
         finally:
-
-            kwargs['logger_func'].info(f"End of request \'{kwargs['request'].url._url}\'.")
+            kwargs['logger_func'].info('End of request.', kwargs['request'].url._url)
             return rsp
 
     return wrapper
